51f73fa77c52aaa62bd4bdebdd88205c2a893dfc
Julia Bouvier <juliabouv@gmail.com>
2019-10-23 10:35:54 -0700

    Created Wizard model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_173253) do
+ActiveRecord::Schema.define(version: 2019_10_23_173458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2019_10_23_173253) do
     t.string "description"
     t.integer "stock"
     t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wizards", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
