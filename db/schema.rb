c1f3c86a475c538692a6c073b8627e96a0d269a3
Julia Bouvier <juliabouv@gmail.com>
2019-10-23 11:03:35 -0700

    Connected Order and OrderItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_175827) do
+ActiveRecord::Schema.define(version: 2019_10_23_180134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_175827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
@@ -71,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_175827) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "products", "wizards"
   add_foreign_key "reviews", "products"
