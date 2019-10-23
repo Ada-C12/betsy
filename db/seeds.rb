ee5b4d5894b7fa877bd82bd702d4e07d205490af
Kristina <kristina.tanya@gmail.com>
2019-10-23 15:54:07 -0700

    added products index and seeded with product and merchant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+# seed merchants
+MERCHANT_FILE = Rails.root.join('db', 'seed_data', 'merchants.csv')
+puts "Loading raw merchant data from #{MERCHANT_FILE}"
+
+merchant_failures = []
+CSV.foreach(MERCHANT_FILE, :headers => true) do |row|
+  merchant = Merchant.new
+  merchant.username = row['username']
+  merchant.email= row['email']
+  merchant.uid = row['uid']
+  merchant.provider= row['provider']
+
+  successful = merchant.save
+  if !successful
+    merchant_failures << merchant
+    puts "Failed to save merchant: #{merchant.inspect}"
+    puts "#{merchant.errors.messages}"
+  else
+    puts "Created merchant: #{merchant.inspect}"
+  end
+end
+
+puts "Added #{Merchant.count} merchant records"
+puts "#{merchant_failures.length} merchants failed to save"
+
+# seed products
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
+puts "Loading raw product data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.name = row['name']
+  product.description = row['description']
+  product.price = row['price']
+  product.photo_url = row['photo_url']
+  product.stock = row['stock']
+  product.merchant_id = row['merchant_id']
+
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts "Failed to save product: #{product.inspect}"
+    puts "#{product.errors.messages}"
+  else
+    puts "Created product: #{product.inspect}"
+  end
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} products failed to save"
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,9 +3,15 @@ require "test_helper"
 describe ProductsController do
   describe "index" do
     it "gives back a successful response" do
+      get products_path
+
+      must_respond_with :success
     end
 
-    it "gives back a 404 response if there are no products available" do
+    it "gives back a sucessful response when there is no products" do
+      get products_path
+      
+      must_respond_with :success
     end
   end 
 
