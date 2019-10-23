e312598bb8f5b4555e2cb365d4360c1feb26fd2b
Julia Bouvier <juliabouv@gmail.com>
2019-10-23 10:36:34 -0700

    Created Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_173458) do
+ActiveRecord::Schema.define(version: 2019_10_23_173607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2019_10_23_173458) do
     t.string "description"
     t.integer "stock"
     t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
