7fc3281985adc701749760f44f0cc99b2a24e3df
ohcloud <cloudylopez@icloud.com>
2019-10-23 11:18:37 -0700

    initial first commit of set up, bundle install, created, migrated db and created Orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/update'
+  get 'orders/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
