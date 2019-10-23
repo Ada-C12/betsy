af2ac1b00d781fe9c7320ea88775248465196211
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-23 15:28:34 -0700

    Wrote merchant model method build_from_github.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants, except: [:delete]
   get "/auth/github", as: "github_login"
-  
+  get "/auth/:provider/callback", to: "merchants#create"
+
   root to: 'homepages#index'
 end
