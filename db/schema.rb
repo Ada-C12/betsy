81711e8fa86a3cb7e01c5fecbca13d516e42fbb9
eaball35 <eaball35@gmail.com>
2019-10-23 11:10:57 -0700

    added imgurl column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_174508) do
+ActiveRecord::Schema.define(version: 2019_10_23_180825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_174508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "img_url"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
