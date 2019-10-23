648acdca587098e71ef2e6f1f81ae08955c44db6
Kristina <kristina.tanya@gmail.com>
2019-10-23 14:34:50 -0700

    added a link to products index page from home page

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "gives back a successful response" do
+    end
+
+    it "gives back a 404 response if there are no products available" do
+    end
+  end 
+
+  # show
+
+  # new
+
+  # edit
+
+  # create
+
+  # update
+
+  # destroy
+
+
+
 end
