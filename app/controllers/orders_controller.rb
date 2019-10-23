824b0f9b2039ee8f6c78d7c5e10d071cdb187338
eaball35 <eaball35@gmail.com>
2019-10-22 20:29:52 -0700

    added comments from discussions today + added review model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  def new
+  end
+
+  #(Cart?)
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
 end
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,27 @@
 class ProductsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def new
+    # Only logged in can new
+  end
+
+  def create
+    # Only merchant of product can create
+  end
+
+  def edit
+    # Only merchant of product can update
+  end
+
+  def update
+    # Only merchant of product can update
+  end
+
+  def destroy
+    # Only merchant of product can detroy
+  end
 end
