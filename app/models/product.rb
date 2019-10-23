b72cb8530355ae8f9cb92d402b97e3bad07b39b7
eaball35 <eaball35@gmail.com>
2019-10-23 11:05:30 -0700

    added product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: {scope: :user_id}
+  validates_length_of :name, minimum: 1, maximum: 50
+  validates :category, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :user_id, presence: true
+  
   belongs_to :user
   has_many :order_items
 end
