581ca9791253e1b65509a08bda80d40c7cf80827
Julia Bouvier <juliabouv@gmail.com>
2019-10-23 11:00:41 -0700

    Connected Product and OrderItems

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :wizard
   has_many :reviews
+  has_many :order_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_175511) do
+ActiveRecord::Schema.define(version: 2019_10_23_175827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_10_23_175511) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_10_23_175511) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "products"
   add_foreign_key "products", "wizards"
   add_foreign_key "reviews", "products"
 end
