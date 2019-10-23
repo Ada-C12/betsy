db84413e3db5e4bbfe3c8535564524806eaef5e0
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-10-23 12:19:26 -0700

    made product model, starting on product model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe Product do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    # has_many :reviews
+    # has_many :orderitems
+    # has_many :orders, through: :orderitems
+    # belongs_to :merchant
+    # has_and_belongs_to_many :types
+    describe "belongs to a merchant" do
+      it "can set the merchant through merchant" do
+      end
+      
+      it "can set the merchant through merchant_id" do
+      end
+    end
+  end
+  
+  describe "validations" do
+  end
 end
