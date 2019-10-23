132896255bff5a5e03f97158be4a4e0edb3d695c
eaball35 <eaball35@gmail.com>
2019-10-22 20:34:29 -0700

    added columns to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_021516) do
+ActiveRecord::Schema.define(version: 2019_10_23_033236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2019_10_23_021516) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "rating"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
