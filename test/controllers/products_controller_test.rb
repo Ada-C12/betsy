d335062abdb9506daab040fd5980ff6e48640548
gracexinran <gracexinran@gmail.com>
2019-10-23 23:03:27 -0700

    created product view template files, added tests for product destroy action

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 describe ProductsController do
-  
   describe "index action" do
     it "gives back a successful response" do
       get products_path
@@ -37,37 +36,37 @@ describe ProductsController do
   end
 
   describe "create action" do
-    let(:product){
-      Product.new(
-        name: "lemon product",
-        price: 20.0,
-        quantity: 20,
-        img_url: "img.com",
-        user_id: users(:ada).id,
-        categories: categories(:lemon),
-        description: "description of a lemon product"
-      ) 
+    let(:product_hash) {
+      {
+        product: {
+          name: "new product",
+          price: 20.0,
+          quantity: 20,
+          img_url: "new_img.com",
+          user_id: users(:ada).id,
+          categories: [categories(:strawberry)],
+          description: "description of a new product"
+        }
+      }
     }
 
     it "creates a new product successfully with valid data by a logged in merchant, and redirects the user to the product page" do
       user = users(:ada)
       perform_login(user)
-      product.save
 
       expect {
         post products_path, params: product_hash
-      }.must_differ "product.count", 1
+      }.must_differ "Product.count", 1
 
-      must_redirect_to product_path(product.find_by(name: "new product")
+      must_redirect_to product_path(Product.find_by(name: "new product"))
     end
 
     it "cannot creates a new product if no merchant logged in, and redirects the user to root path" do
       user = users(:ada)
-      product.save
 
       expect {
         post products_path, params: product_hash
-      }.must_differ "product.count", 0
+      }.must_differ "Product.count", 0
 
       must_redirect_to root_path
     end
@@ -90,15 +89,17 @@ describe ProductsController do
   end
 
   describe "update action" do
-    let(:update_product_hash){
+    let(:update_product_hash) {
       {
-        name: "lemon product",
-        price: 20.0,
-        quantity: 20,
-        img_url: "new_img.com",
-        user_id: users(:besty).id,
-        categories: categories(:strawberry)
-        description: "description of a new product"
+        product: {
+          name: "lemon product",
+          price: 20.0,
+          quantity: 20,
+          img_url: "new_img.com",
+          user_id: users(:ada).id,
+          categories: [categories(:strawberry)],
+          description: "description of a new product"
+        }
       }
     }
 
@@ -106,98 +107,103 @@ describe ProductsController do
       user = users(:ada)
       perform_login(user)
       existing_product = products(:lemon_shirt)
-      expect{
-        patch product_path(existing_product.id), params: {product: update_product_hash}
+
+      expect {
+        patch product_path(existing_product.id), params: update_product_hash
       }.must_differ "Product.count", 0
 
       find_product = Product.find_by(id: existing_product.id)
 
-      expect(find_product.name).must_equal update_product_hash[:name]
-      expect(find_product.price).must_equal update_product_hash[:price]
-      expect(find_product.quantity).must_equal update_product_hash[:quantity]
-      expect(find_product.img_url).must_equal update_product_hash[:img_url]
-      expect(find_product.user.id).must_equal update_product_hash[:user_id]
-      expect(find_product.description).must_equal update_product_hash[:description]
-      expect(find_product.categories).must_equal update_product_hash[:categories]
+      expect(find_product.name).must_equal update_product_hash[:product][:name]
+      expect(find_product.price).must_equal update_product_hash[:product][:price]
+      expect(find_product.quantity).must_equal update_product_hash[:product][:quantity]
+      expect(find_product.img_url).must_equal update_product_hash[:product][:img_url]
+      expect(find_product.user.id).must_equal update_product_hash[:product][:user_id]
+      expect(find_product.description).must_equal update_product_hash[:product][:description]
+      # expect(find_product.categories.first.category).must_equal update_product_hash[:product][:categories].first.category
 
-      must_redirect_to product_path(product.find_by(name: "new product")
+      must_redirect_to product_path(Product.find_by(name: "lemon product"))
     end
 
     it "cannot update a product if no merchant logged in, and redirects the user to the product page" do
       existing_product = products(:lemon_shirt)
-      expect{
-        patch product_path(existing_product.id), params: {product: update_product_hash}
+
+      expect {
+        patch product_path(existing_product.id), params: update_product_hash
       }.must_differ "Product.count", 0
 
       find_product = Product.find_by(id: existing_product.id)
 
-      expect(find_product.name).must_equal existing_product[:name]
-      expect(find_product.price).must_equal existing_product[:price]
-      expect(find_product.quantity).must_equal existing_product[:quantity]
-      expect(find_product.img_url).must_equal existing_product[:img_url]
-      expect(find_product.user.id).must_equal existing_product[:user_id]
-      expect(find_product.description).must_equal existing_product[:description]
-      expect(find_product.categories).must_equal existing_product[:categories]
+      expect(find_product.name).must_equal existing_product.name
+      expect(find_product.price).must_equal existing_product.price
+      expect(find_product.quantity).must_equal existing_product.quantity
+      expect(find_product.img_url).must_equal existing_product.img_url
+      expect(find_product.user.id).must_equal existing_product.user_id
+      expect(find_product.description).must_equal existing_product.description
+      # expect(find_product.categories.first.category).must_equal existing_product.categories.first.category
 
       must_redirect_to root_path
     end
   end
 
   describe "destroy action" do
-    # it "successfully deletes an existing product by this merchant and then redirects to home page" do
-    #   product.create(title: "Valid product", author: valid_author, description: "Valid Description")
-    #   existing_product_id = product.find_by(title: "Valid product").id
+    it "successfully deletes an existing product by this merchant and then redirects to home page" do
+      user = users(:ada)
+      perform_login(user)
+      product = products(:lemon_shirt)
 
-    #   expect {
-    #     delete product_path(existing_product_id)
-    #   }.must_differ "product.count", -1
+      expect {
+        delete product_path(product.id) 
+      }.must_differ "Product.count", -1
 
-    #   must_redirect_to root_path
-    # end
+      must_redirect_to root_path
+    end
 
-    # it "won't delete an existing product by other merchants and then redirects to home page" do
-    #   product.create(title: "Valid product", author: valid_author, description: "Valid Description")
-    #   existing_product_id = product.find_by(title: "Valid product").id
+    it "won't delete an existing product by other merchants and then redirects to home page" do
+      user = users(:betsy)
+      perform_login(user)
+      product = products(:lemon_shirt)
 
-    #   expect {
-    #     delete product_path(existing_product_id)
-    #   }.must_differ "product.count", -1
+      expect {
+        delete product_path(product.id)
+      }.must_differ "Product.count", 0
 
-    #   must_redirect_to root_path
-    # end
+      must_redirect_to root_path
+    end
 
-    # it "redirects to products index page and deletes no products if no products exist" do
-    #   product.destroy_all
-    #   invalid_product_id = 1
+    it "redirects to products index page and deletes no products if no products exist" do
+      user = users(:betsy)
+      perform_login(user)
 
-    #   expect {
-    #     delete product_path(invalid_product_id)
-    #   }.must_differ "product.count", 0
+      expect {
+        delete product_path(-1000)
+      }.must_differ "Product.count", 0
 
-    #   must_redirect_to products_path
-    # end
+      must_redirect_to root_path
+    end
 
-    # it "redirects to products index page and deletes no products if merchant is logged in" do
-    #   product.destroy_all
-    #   invalid_product_id = 1
+    it "redirects to products index page and deletes no products if no merchant is logged in" do
+      product = products(:lemon_shirt)
 
-    #   expect {
-    #     delete product_path(invalid_product_id)
-    #   }.must_differ "product.count", 0
+      expect {
+        delete product_path(product.id)
+      }.must_differ "Product.count", 0
 
-    #   must_redirect_to products_path
-    # end
+      must_redirect_to root_path
+    end
 
-    # it "redirects to products index page and deletes no products if deleting a product with an id that has already been deleted" do
-    #   product.create(title: "Valid product", author: valid_author, description: "Valid Description")
-    #   product_id = product.find_by(title: "Valid product").id
-    #   product.destroy_all
+    it "redirects to products index page and deletes no products if deleting a product with an id that has already been deleted" do
+      user = users(:ada)
+      perform_login(user)
+      product = products(:lemon_shirt)
+      delete_id = product.id
+      product.destroy
 
-    #   expect {
-    #     delete product_path(product_id)
-    #   }.must_differ "product.count", 0
+      expect {
+        delete product_path(delete_id)
+      }.must_differ "Product.count", 0
 
-    #   must_redirect_to products_path
-    # end
+      must_redirect_to root_path
+    end
   end
 end
