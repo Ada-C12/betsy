2df014de864b831a6235c95321ad68d7134a924c
dora1405 <li.dora05@gmail.com>
2019-10-24 10:09:45 -0700

    all product images

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,19 +1,29 @@
 require "test_helper"
 
 describe ProductsController do
+
+  let (:product) {
+    Product.create name: "ice sword", price: 1500, description: "wielded by Jack Frost", stock: 10, wizard: "zedzorander", categories: "artifact"
+  }
   describe "index action" do
 
     it "gives back a successful response" do
-      # Arrange
-      # ... Nothing right now!
 
-      # Act
-      # Send a specific request... a GET request to the path "/books"
       get products_path
 
-      # Assert
-      # The response was OK!
       must_respond_with :success
+    end
+
+    it "gives back a successful response if there are no products" do
+      
+      Product.destroy_all
+      # product = products(:product1)
+      # no_product = product.destroy
+
+      get products_path
+
+      must_respond_with :success
+
     end
 
   end
