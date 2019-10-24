36c77527bc1557f04825d00d202a8ce2c981eb44
Kristina <kristina.tanya@gmail.com>
2019-10-23 22:21:08 -0700

    passing nominal test case for product#create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,12 +20,27 @@ class ProductsController < ApplicationController
   end
 
   # only merchants
-  def edit
-  end 
+  def create
+    @product = Product.new(product_params)
+
+
+    p @product.inspect
+    if @product.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created #{@product} #{@product.id}"
+      redirect_to product_path(@product)
+    else
+      p @product.errors.messages
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@product}"
+      flash[:messages] = @product.errors.messages
+      render :new, status: :bad_request
+    end
+  end
 
   # only merchants
-  def create
-  end
+  def edit
+  end 
 
   # only merchants
   def update
@@ -38,7 +53,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :photo_url, :stock, :merchant)
+    params.require(:product).permit(:name, :description, :price, :photo_url, :stock, :merchant_id)
   end
 
 
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -31,11 +31,46 @@ describe ProductsController do
     end
   end
 
-  # new
+  describe "new" do
+    it "can get the new product page" do
+      get new_product_path
+      must_respond_with :success
+    end 
+  end
+
+  describe "create" do
+    it "creates a product sucessfully with valid data, and redirects the user to the product page" do
+      merchant = Merchant.create(username: "jake", email: "jakw123@gmail.com", uid: 12232234, provider: "github")
+
+      product_hash = {
+        product: {
+          name: "Growler",
+          description: "Keeps your beer fresh!",
+          price: 30.0,
+          photo_url: "https://images-na.ssl-images-amazon.com/images/I/51NR%2BJ9lLNL._SY679_.jpg",
+          stock: 5,
+          merchant_id: merchant.id, 
+        }
+      }
+
+      # p product_hash[:product].errors.messages
+      p merchant.inspect
+      
+      expect {
+        post products_path, params: product_hash
+      }.must_differ 'Product.count', 1
+
+      new_product = Product.find_by(name: product_hash[:product][:name])
+      expect(new_product.name).must_equal product_hash[:product][:name]
+
+    end
+
+    it "does not create a product with invalid data, and renders edit form" do
+    end
+
+  end
 
   # edit
-
-  # create
 
   # update
 
