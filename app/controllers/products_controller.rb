d9e59205792d87bdc967761d581f96ad9a9d259c
Julia Bouvier <juliabouv@gmail.com>
2019-10-24 11:42:09 -0700

    Updated products by wizards to be a nested route in product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    wizard_id = params[:wizard_id]
+    
+    if wizard_id.nil?
+      @products = Product.all
+    else
+      @wizard = Wizard.find_by(id: wizard_id)
+      if @wizard
+        @products = @wizard.products
+      else
+        head :not_found
+      end
+    end
   end
-
-
+  
+  
 end
