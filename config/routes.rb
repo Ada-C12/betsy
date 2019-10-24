089b4b6a2299b2a44d10e31c809510b1ef598d93
geli-gel <angele.zam@gmail.com>
2019-10-23 23:47:35 -0700

    added category_products nested route and changed root to products#index for my own testing - ok to change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "wizards#products"
+  root to: "products#index"
   
   get "/wizards/:id/products", to: "wizards#products", as: "wizards_products"
+
+  resources :products
+  resources :categories do
+    resources :products, only: :index
+  end
+
 end
