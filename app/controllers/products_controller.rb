97737360d78c440c4687f49c6d1fa8ba3a3fa90c
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-10-24 07:06:26 -0700

    added routes, show action, view, and tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+    def show 
+        @product = Product.find_by(id: params[:id])
+        if @product.nil?
+            head :not_found 
+            return 
+        end 
+    end 
+    
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let(:product) { products(:product1) }
+
+  describe "show" do
+    it "responds with success when showing an existing valid product" do
+      get product_path(product.id)
+
+      must_respond_with :success
+    end
+
+    it "responds with 404 with an invalid product id " do
+      get product_path(-1)
+
+      must_respond_with :not_found
+    end
+  end
 end
