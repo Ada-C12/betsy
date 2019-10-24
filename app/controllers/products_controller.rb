7085b0a17ff7ae7b5f30d8f82881aee6eeb1aad4
geli-gel <angele.zam@gmail.com>
2019-10-23 23:43:48 -0700

    added controller tests for viewing products#index by category_id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+
+  # Can now be accessed via /, /books, or /authors/:author_id/books
+  def index
+    category_id = params[:category_id]
+    if category_id.nil?
+      @products = Product.all
+    else 
+      @category = Category.find_by(id: category_id)
+      if @category
+        @products = @category.products
+      else
+        render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+      end
+    end
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,26 @@
 require "test_helper"
 
 describe ProductsController do
+  describe "index action" do
+
+    describe "index action given category_id" do
+        
+      it "gives back a success response if given a valid category id" do
+        valid_category = Category.first
+        assert_not_nil(valid_category)
+        get category_products_path(valid_category.id)
+        must_respond_with :success
+      end
+
+      it "gives back a not found response if given an invalid category id" do
+        invalid_category_id = -1
+        invalid_category = Category.find_by(id: invalid_category_id)
+        assert_nil(invalid_category)
+        get category_products_path(invalid_category_id)
+        must_respond_with :not_found
+      end
+    end
+  end
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
