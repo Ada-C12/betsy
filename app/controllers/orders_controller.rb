fc65ef317ea033273b5b432a4ca37375e75cad31
gracexinran <gracexinran@gmail.com>
2019-10-25 16:23:44 -0700

    replaced new with cart in orders controller, refactored order model validations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,24 +2,48 @@ class OrdersController < ApplicationController
   skip_before_action :require_login
   
   def show
-    # find current order in session
-    @order = Order.find_by(id: session[:cart_id])
+    @order = Order.find_by(id: params[:id]])
+    if @order.nil?
+      flash[:error] = "Order doesn't exist!"
+      redirect_to root_path
+    end
   end
-
-  def new
+  
+  def cart
     if session[:cart_id].nil?
-      @order = Order.new
+      @order = Order.create(status: "pending")
       session[:cart_id] = @order.id
     else
       @order = Order.find_by(id: session[:cart_id])
+      if @order.nil?
+        flash[:error] = "Order doesn't exist!"
+        redirect_to root_path
+      end
     end
   end
-
+  
   def create
+    @order = Order.update(order_params)
+    @order.status = "paid"
+    @order.customer_id = session[:user_id]
+    if @order.save
+      session[:cart_id] = nil
+      flash[:success] = "Order #{@order.id} has been successfully created!"
+      redirect_to order_path(@order.id)
+    else
+      flash[:error] = "Something went wrong! Order was not paid."
+      render cart_path
+    end
   end
-
+  
   def destroy
   end
+  
+  private
 
+  def order_params
+    return params.require(:order).permit(:name, :email, :address, :cc_name, :cc_last4, :cc_exp, :cc_cvv, :billing_zip)
+  end
+  
 end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,17 @@
 class Order < ApplicationRecord
   has_many :order_items
 
-  validates :status, presence: true 
-  validates :name, presence: true
-  validates :email, presence: true, format: { with: /@/, message: "Email format must be valid." }
-  validates :address, presence: true 
-  validates :cc_name, presence: true 
-  validates_numericality_of :cc_last4, within: 1000..9999
-  validates :cc_exp, presence: true 
-  validates_numericality_of :cc_cvv, greater_than: 0
-  validates :billing_zip, presence: true 
+  validates :status, presence: true
+  validates :name, presence: true, if: :not_pending?
+  validates :email, presence: true, format: { with: /@/, message: "Email format must be valid." } , if: :not_pending?
+  validates :address, presence: true, if: :not_pending?
+  validates :cc_name, presence: true, if: :not_pending? 
+  validates_numericality_of :cc_last4, within: 1000..9999, if: :not_pending?
+  validates :cc_exp, presence: true, if: :not_pending?
+  validates_numericality_of :cc_cvv, greater_than: 0, if: :not_pending?
+  validates :billing_zip, presence: true, if: :not_pending?
   
+  def not_pending?
+    status != 'pending'
+  end
 end
