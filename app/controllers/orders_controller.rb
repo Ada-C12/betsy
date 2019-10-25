6fc1ffe1fad4f8cb24649d4e5d26a6812d6ccd0d
gracexinran <gracexinran@gmail.com>
2019-10-25 10:45:37 -0700

    added relationships and validations to order and order_item, added customer_id column to orders, added show and new actions to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,17 @@
 class OrdersController < ApplicationController
-  def new
+  def show
+    # find current order in session
+    @order = Order.find_by(id: session[:cart_id])
+    
   end
 
-  #(Cart?)
-  def show
+  def new
+    if session[:cart_id].nil?
+      @order = Order.new
+      session[:cart_id] = @order.id
+    else
+      @order = Order.find_by(id: session[:cart_id])
+    end
   end
 
   def create
