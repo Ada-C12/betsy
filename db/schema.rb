cf56755f17f3ad3f9f4f51ce4d4f74953a3c0664
Julia Bouvier <juliabouv@gmail.com>
2019-10-25 14:58:59 -0700

    Updated retired to correct model with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_000720) do
+ActiveRecord::Schema.define(version: 2019_10_25_215449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_10_24_000720) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "retired", default: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -63,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_10_24_000720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "wizard_id"
+    t.boolean "retired", default: false
     t.index ["wizard_id"], name: "index_products_on_wizard_id"
   end
 
