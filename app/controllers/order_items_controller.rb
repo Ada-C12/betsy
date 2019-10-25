d5365aa9ba8d12741ec258fc39c89f0ed9ea55fd
Julia Bouvier <juliabouv@gmail.com>
2019-10-25 15:32:38 -0700

    OrderItem creation implemented. Order creation needs to be troubleshooted

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,10 +6,17 @@ class OrderItemsController < ApplicationController
       return head :not_found 
     end
 
+    unless session[:order_id]
+      order = Order.create
+    else
+      order = Order.find_by(id: session[:order_id])
+    end
 
-    # Order.create
-
-    # @order_item = OrderItem.new(order_items_params).merge(shipped: false, product_id: product.id, order_id:)
+    @order_item = OrderItem.new(order_items_params)
+    @order_item.shipped = false
+    @order_item.product = product
+    @order_item.order = order
+    @order_item.save
 
     if @order_item.save
       flash[:success] = "Item added to cart!"
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,23 @@
 class OrdersController < ApplicationController
   def create
     @order = Order.new(status: pending)
-
+    
+    if @order.save
+      session[:order_id] = @order.id
+      flash[:success] = "Order Created"
+      redirect_back(fallback_location: :back)
+      return
+    else
+      redirect_to root_path
+      flash[:error] = "Error creating order SOMETHING IS WRONG HEREEE"
+      return
+    end
+    
+    
   end
-
-
+  
+  
   # checkout form will be the edit form
-
+  
   # checkout button will be the update action
 end
