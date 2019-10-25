28eb14bf9452000092996ec70f083a4dd32a7582
dora1405 <li.dora05@gmail.com>
2019-10-25 14:49:47 -0700

    minor merge conflict edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :wizard
-<<<<<<< HEAD
   has_many :reviews, dependent: :nullify
   has_many :order_items, dependent: :nullify
   has_and_belongs_to_many :categories, dependent: :nullify
@@ -10,5 +9,5 @@ class Product < ApplicationRecord
   def self.five_products
     return Product.all.sample(5)
   end
-  
+
 end
