655fb94397535f41156150350339230584b406ea
dora1405 <li.dora05@gmail.com>
2019-10-25 18:09:20 -0700

    product model testing validations for name, price, stock and wizard

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,16 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :nullify
   has_and_belongs_to_many :categories, dependent: :nullify
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :stock, presence: true
+  validates :stock, numericality: { only_integer: true }
+  validates :wizard, presence: true
+
+
+
+
   validates_inclusion_of :retired, in: [true, false]
 
   def self.five_products
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,73 @@ require "test_helper"
 
 describe Product do
   describe "validations" do
+
+    it "must have a name, price, stock, wizard, categories and retired information" do
+      is_valid = products(:product1).valid?
+      assert(is_valid)
+    end
+
+    it "is invalid if there is no name" do
+      is_invalid = products(:product1)
+      is_invalid.name = nil
+
+      refute(is_invalid.valid?)
+      expect(is_invalid.errors.messages).must_include :name
+      expect(is_invalid.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "is invalid if there is no price" do
+      is_invalid = products(:product1)
+      is_invalid.price = nil
+
+      refute(is_invalid.valid?)
+      expect(is_invalid.errors.messages).must_include :price
+      expect(is_invalid.errors.messages[:price]).must_equal ["can't be blank", "is not a number"]
+
+    end
+
+    it "is invalid if price is not an integer" do
+      is_invalid = products(:product1)
+      is_invalid.price = "a"
+  
+      refute(is_invalid.valid?)
+      expect(is_invalid.errors.messages).must_include :price
+      expect(is_invalid.errors.messages[:price]).must_equal ["is not a number"]
+
+    end
+
+
+    it "is invalid if there is no stock" do
+      is_invalid = products(:product1)
+      is_invalid.stock = nil
+
+      refute(is_invalid.valid?)
+      expect(is_invalid.errors.messages).must_include :stock
+      expect(is_invalid.errors.messages[:stock]).must_equal ["can't be blank", "is not a number"]
+
+    end
+
+    it "is invalid if stock is not an integer" do
+      is_invalid = products(:product1)
+      is_invalid.stock = "a"
+  
+      refute(is_invalid.valid?)
+      expect(is_invalid.errors.messages).must_include :stock
+      expect(is_invalid.errors.messages[:stock]).must_equal ["is not a number"]
+
+    end
+
+    it "is invalid if there is no wizard" do
+        is_invalid = products(:product1)
+        is_invalid.wizard = nil
+  
+        refute(is_invalid.valid?)
+        expect(is_invalid.errors.messages).must_include :wizard
+        expect(is_invalid.errors.messages[:wizard]).must_equal ["must exist", "can't be blank"]
+  
+    end
+
+
     it "must have a retired" do
       is_invalid = products(:product1)
       is_invalid.retired = nil
