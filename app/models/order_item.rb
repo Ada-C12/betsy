ab98deb6fa25b1bf1cd99337602e62fb1e09bf77
geli-gel <angele.zam@gmail.com>
2019-10-28 15:27:45 -0700

    updated order_item.subtotal and order.total to use Money so that it displays properly in cart view

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,7 +6,8 @@ class OrderItem < ApplicationRecord
   validates :quantity, :numericality => {:only_integer => true, greater_than: 0}
 
   def subtotal
-    return self.quantity * self.product.price_cents
+     subtotal_cents = self.quantity * self.product.price_cents
+     return Money.new(subtotal_cents)
   end
 
 end
