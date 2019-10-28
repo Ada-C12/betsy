dd0084a3f677122f2772bd5866851d1a1690cad3
Kristina <kristina.tanya@gmail.com>
2019-10-28 15:52:58 -0700

    added simplecov & added a 2 column view for a product show page

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/' # for rspec
+  add_filter '/test/' # for minitest
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -5,6 +13,9 @@ require 'minitest/rails'
 require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
