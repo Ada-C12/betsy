74ad78a7752fabe9418104b9fe5b485e56b8eb42
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-10-27 17:07:45 -0700

    updated merchant controller tests.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,26 +6,31 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
-  def show
-    @merchant = Merchant.find_by(id: session[:merchant_id])
-    if @merchant.nil?
-      flash[:warning] = "Merchant with id #{params[:id]} was not found."
-      redirect_to root_path
-    end
-  end
+  # def show
+  #   @merchant = Merchant.find_by(id: session[:merchant_id])
+  #   if @merchant.nil?
+  #     flash[:status] = :failure
+  #     flash[:result_text] = "Merchant with id #{params[:id]} was not found."
+  #     redirect_to root_path
+  #   end
+  # end
 
   def create
     auth_hash = request.env["omniauth.auth"]
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
 
     if merchant
-      flash[:success] = "Logged in as returning merchant #{merchant.username}."
+      flash[:status] = :success
+      flash[:result_text] = "Logged in as returning merchant #{merchant.username}."  
     else
       merchant = Merchant.build_from_github(auth_hash)
       if merchant.save
-        flash[:success] = "Logged in as new merchant #{merchant.username}."
+        flash[:status] = :success
+        flash[:result_text] = "Logged in as new merchant #{merchant.username}." 
       else
-        flash[:error] = "Could not create new merchant account: #{merchant.errors.messages}"
+        flash[:status] = :failure
+        flash[:result_text] = "Could not create new merchant account"
+        flash[:messages] = merchant.errors.messages
         return redirect_to root_path
       end 
     end
@@ -38,7 +43,8 @@ class MerchantsController < ApplicationController
     @current_merchant = Merchant.find_by(id: session[:merchant_id])
 
     unless @current_merchant
-      flash[:error] = "You must be a logged in authorized merchant to access this page"
+      flash[:status] = :failure
+      flash[:result_text] = "You must be a logged in authorized merchant to access this page"
       redirect_to root_path
     end
   end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -9,19 +9,10 @@ describe MerchantsController do
       perform_login(merchants(:brad))
     end
 
-    describe "show" do
+    describe "current" do
       it "responds with success when given current merchants id" do
-        get merchant_path(:brad)
+        get current_merchant_path
         must_respond_with :success
-      end
-  
-      it "responds with a not found flash and redirects to root path when id given does not exist" do
-        merchant_id = -1
-        get merchant_path(merchant_id)
-  
-        expect(flash[:warning]).must_equal "Merchant with id #{merchant_id} was not found."
-        must_respond_with :redirect
-        must_redirect_to root_path
       end
     end
   end
@@ -32,7 +23,6 @@ describe MerchantsController do
         get merchants_path
         must_respond_with :success
       end
-    
     
       it "responds with success when there are no merchants" do
       # merchants = Merchant.all
@@ -53,9 +43,10 @@ describe MerchantsController do
         perform_login(merchant)
   
         must_redirect_to root_path
-        session[:merchant_id].must_equal merchant.id
-        Merchant.count.must_equal start_count
-        expect(flash[:success]).must_equal "Logged in as returning merchant #{merchant.username}."
+        expect(session[:merchant_id]).must_equal merchant.id
+        expect(Merchant.count).must_equal start_count
+        expect(flash[:status]).must_equal :success
+        expect(flash[:result_text]).must_equal "Logged in as returning merchant #{merchant.username}."
       end
   
       it "creates an account for a new merchant and redirects to the root route" do 
@@ -64,12 +55,13 @@ describe MerchantsController do
   
         perform_login(merchant)
   
-        Merchant.count.must_equal start_count + 1
+        expect(Merchant.count).must_equal start_count + 1
   
         merchant = Merchant.find_by(uid: merchant.uid)
         must_redirect_to root_path
-        session[:merchant_id].must_equal merchant.id
-        expect(flash[:success]).must_equal "Logged in as new merchant #{merchant.username}."
+        expect(session[:merchant_id]).must_equal merchant.id
+        expect(flash[:status]).must_equal :success
+        expect(flash[:result_text]).must_equal "Logged in as new merchant #{merchant.username}."
   
       end
   
@@ -87,23 +79,23 @@ describe MerchantsController do
   
         must_redirect_to root_path
         expect(session[:merchant_uid]).must_be_nil
-  
-        # expect(flash[:success]).must_equal "Could not create new merchant account: #{merchant.errors.messages}"
-  
-        # merchant = Merchant.new
-        # OmniAuth.config.mock_auth[:github] = 
-        # OmniAuth::AuthHash.new(mock_auth_hash(merchant))
-        
-        # expect {
-        #   get auth_callback_path(:github)
-        # }.wont_change "Merchant.count"
-  
-        # must_redirect_to root_path
-        # expect(session[:merchant_id]).must_be_nil
-        # expect(flash[:success]).must_equal "Could not create new merchant account: #{merchant.errors.messages}"
-  
       end
     end
+
+    describe "current" do 
+      it "responds with a not found flash and redirects to root path when id given does not exist" do
+       
+        get current_merchant_path
+
+        # expect(flash[:status]).must_equal :failure
+        # expect(flash[:result_text]).must_equal "You must be a logged in authorized merchant to access this page"
+  
+        
+        must_respond_with :redirect
+        must_redirect_to root_path
+      end
+    end
+      
   end
 
 end
