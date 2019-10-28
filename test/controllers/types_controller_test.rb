f9dbe20d7625afc72d69cf7daa96f162aa0eaf46
ohcloud <cloudylopez@icloud.com>
2019-10-28 11:48:46 -0700

    testing

--- test/controllers/types_controller_test.rb
+++ test/controllers/types_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe TypesController do
-  let (:existing_type) {types ()}
-  
-  
+  let (:existing_type) {types (:lager)}
 end
+describe "show" do 
+  it "will responds with success when the given type_id exist"
+  
+  valid_type = Type.create(:lager)
+  
+  get types_path(valid_type.id)
+  must_respond_with :success
+end
+
+
+
+
+end#end of types controller
