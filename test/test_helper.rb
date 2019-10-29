3e16d9a3b04a6612946b4053dfc62bcd3e52901b
Kristina <kristina.tanya@gmail.com>
2019-10-29 13:53:53 -0700

    finished merchant order fulfillment page, need to finish writing test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ SimpleCov.start 'rails' do
   add_filter '/test/' # Tests should not be counted toward coverage.
   add_filter '/bin/'
   add_filter '/db/'
+  add_filter '/spec/'
 end
 
 ENV['RAILS_ENV'] ||= 'test'
