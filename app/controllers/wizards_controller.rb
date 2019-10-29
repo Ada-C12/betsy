95e96e3ceceb3dc7aee2549ff851bc0dd47d9bb8
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-10-29 13:42:16 -0700

    added order to wizard model and view

--- app/controllers/wizards_controller.rb
+++ app/controllers/wizards_controller.rb
@@ -1,11 +1,12 @@
 class WizardsController < ApplicationController
-
-
-
-def show 
+  def show
     # @wizard = Wizard.find_by(id: session[:wizard_id])
     @wizard = Wizard.find_by(id: params[:id])
-end 
 
+    @orders = @wizard.orders
 
+    if params[:status]
+      @orders = @wizard.orders.select { |order| order.status == params[:status] }
+    end
+  end
 end
--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -1,26 +1,42 @@
 class Wizard < ApplicationRecord
   has_many :products
-  
+
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  
 
+  def total_revenue
+    total = 0
+    self.products.each do |product|
+      product.order_items.each do |item|
+        total += item.subtotal
+      end
+    end
+    return total
+  end
 
-def total_revenue 
-  total = 0 
-  self.products.each do |product|
-    product.order_items.each do |item|
-      total += item.subtotal 
-    end 
-  end 
+  def total_revenue_by_status(status)
+    total = 0
+    self.products.each do |product|
+      product.order_items.each do |item|
+        if item.order.status == status
+          total += item.subtotal
+        end
+      end
+    end
+    return total
+  end
 
-end 
-  
+  def orders
+    orders = []
 
-
-
-
+    self.products.each do |product|
+      product.order_items.each do |order_item|
+        orders << order_item.order
+      end
+    end
+    return orders.uniq
+  end
 end
