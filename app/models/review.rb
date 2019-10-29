8e863cb8f675ab85a0cd90bfd5aac45ffd0ee204
eaball35 <eaball35@gmail.com>
2019-10-28 20:22:08 -0700

    added testing on review relationships

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :description, presence: true
   validates_length_of :description, minimum: 1, maximum: 350
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: {scope: :product_id}
   validates :product_id, presence: true
   
   belongs_to :product
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -16,20 +16,6 @@ describe Review do
   end
 
   describe "relationships" do
-    # it "can have many order items" do   
-    #   product = Product.create(
-    #     name: "Orange Soap Dispenser",
-    #     price: 20.00,
-    #     quantity: 15,
-    #     img_url: "http:\\fakeurl.com",
-    #     user: users(:betsy),
-    #     description: "A soap dispenser shaped like an orange."
-    #   )
-
-    #   product.order_items << OrderItem.create(quantity: 2, product: product, order: Order.first)
-    #   product.order_items << OrderItem.create(quantity: 3, product: product, order: Order.last)
-
-    #   expect(product.order_items.count).must_equal 2
-    # end
-end
+    
+  end
 end
