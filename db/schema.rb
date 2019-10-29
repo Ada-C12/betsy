2b2ff5b013c141d9a971147713db8bbc82450b2d
eaball35 <eaball35@gmail.com>
2019-10-28 21:47:03 -0700

    added migration for active column in products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_034632) do
+ActiveRecord::Schema.define(version: 2019_10_29_044211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_034632) do
     t.bigint "user_id"
     t.string "img_url"
     t.string "description"
+    t.boolean "active"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
