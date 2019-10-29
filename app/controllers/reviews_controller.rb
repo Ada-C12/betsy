124a42014cf39a611804590b3821300ef58da3f2
eaball35 <eaball35@gmail.com>
2019-10-29 10:42:37 -0700

    Added testing on review create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,9 @@
 class ReviewsController < ApplicationController
-  #skip_before_action :require_login, :only => [:create, :show]
+  skip_before_action :require_login, :only => [:create, :show]
   skip_before_action :find_order
-
-  def new
-    @review = Review.new
-  end
-
+  
   def create
-    @review = Review.new(reivew_params)
+    @review = Review.new(review_params)
 
     if @review.save
       flash[:success] = "Your review has been added successfully"
@@ -16,7 +12,7 @@ class ReviewsController < ApplicationController
       return
     else
       flash.now[:error] = "Something went wrong! Review was not added."
-      render :new
+      redirect_to root_path
       return
     end
   end
@@ -65,6 +61,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    return params.require(:review).permit(:rating, :description)
+    return params.require(:review).permit(:rating, :description, :user_id, :product_id)
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,6 +1,53 @@
 require "test_helper"
 
 describe ReviewsController do
-  describe "new" do
+  describe "create action" do
+    let(:lemon_shirt) {
+      products(:lemon_shirt)
+    }
+    let(:betsy) {
+      users(:betsy)
+    }
+    
+    let(:review_hash) {
+      {
+        review: {
+          rating: 2,
+          description: "description of a new product",
+          user_id: betsy.id,
+          product_id: lemon_shirt.id,
+        }
+      }
+    }
+
+    let(:invalid_review_hash) {
+      {
+        review: {
+          rating: 10,
+          description: "",
+          user_id: -1,
+          product_id: -1,
+        }
+      }
+    }
+
+    it "creates a new review successfully with valid data, and redirects the user to the product page" do
+      expect {
+        post reviews_path, params: review_hash
+      }.must_differ "Review.count", 1
+
+      must_redirect_to product_path(lemon_shirt.id)
+    end
+
+    it "does not create new review when passed invalid data, and redirects user to the product page" do
+      expect {
+        post reviews_path, params: invalid_review_hash
+      }.must_differ "Review.count", 0
+
+      must_redirect_to root_path
+    end
   end
+
+  
+
 end
