3310ad7b41c0c2539e4184675e645af901ff942e
geli-gel <angele.zam@gmail.com>
2019-10-29 22:20:46 -0700

    added back build_from_github from master which was accidentally removed from wizard.rb

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -6,5 +6,15 @@ class Wizard < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  def self.build_from_github(auth_hash)
+    wizard = Wizard.new
+    wizard.uid = auth_hash[:uid]
+    wizard.provider = "github"
 
+    wizard.username = auth_hash[:info][:nickname]
+    wizard.email = auth_hash[:info][:email]
+    return wizard
+  end
+  
 end
