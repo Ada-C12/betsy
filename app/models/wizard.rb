80e398f93b14cb4cfe1ab5ed56154326b251f27a
geli-gel <angele.zam@gmail.com>
2019-10-29 22:16:16 -0700

    removed extra whitespace

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -6,9 +6,5 @@ class Wizard < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  
-
-
-
 
 end
