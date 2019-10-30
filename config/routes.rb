53e20a52dc83df270630f5eecba3797f12fe901f
geli-gel <angele.zam@gmail.com>
2019-10-29 22:15:50 -0700

    trying to revert the merge with remote branch i did earlier to be able to place in my add new product button

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   resources :order_items, only: [:update, :destroy]
   get "/cart", to: "orders#cart", as: "cart"
   
-  resources :wizards, only: [:show]
   resources :wizards do
     resources :products, only: [:index, :new, :create]
   end
