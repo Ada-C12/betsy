8f721633d163a5b6f4da6afeb36c53739c18b6c6
gracexinran <gracexinran@gmail.com>
2019-10-29 18:47:57 -0700

    implemented function to filter order_items by order status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,20 @@ class UsersController < ApplicationController
       return
     end
   end
-
+  
   def current
+    if params[:status].nil?
+      @order_items = []
+      @current_user.products.each do |product|
+        product.order_items.each do |order_item|
+          @order_items << order_item
+        end
+      end
+    else
+      @order_items = filter_orderitems(params[:status])
+    end
   end
-
+  
   def create
     auth_hash = request.env["omniauth.auth"]
     user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
@@ -31,10 +41,10 @@ class UsersController < ApplicationController
     session[:user_id] = user.id 
     redirect_to root_path
   end
-
+  
   def edit
   end
-
+  
   def update
     if @current_user.update(user_params)
       flash[:success] = "User data updated successfully."
@@ -44,16 +54,27 @@ class UsersController < ApplicationController
       return render :edit
     end
   end
-
+  
   def destroy
     session[:user_id] = nil
     flash[:success] = "Successfully logged out!"
     redirect_to root_path 
   end 
-
+  
   private
-
+  
   def user_params
     return params.require(:user).permit(:uid, :merchant_name, :email, :provider, :username)
   end
+  
+  def filter_orderitems(status)
+    order_items = []
+    @current_user.products.each do |product|
+      product.order_items.each do |order_item|
+        order_items << order_item if order_item.order.status == status
+      end
+    end
+    return order_items
+  end
+
 end
