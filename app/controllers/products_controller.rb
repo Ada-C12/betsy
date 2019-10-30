05ccb12f3523aa6dd25e94eab0628ae110422af7
Paige Davenport <davenport.paige@outlook.com>
2019-10-30 09:48:02 -0700

    updated redirect for successful product update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -63,7 +63,7 @@ class ProductsController < ApplicationController
   def update
     if @product.update(product_params)
       flash[:success] = "Product #{@product.name} has been updated successfully"
-      redirect_to product_path(@product.id)
+      redirect_to current_user_path
       return
     else
       flash.now[:error] = "Something went wrong! Product can not be edited."
