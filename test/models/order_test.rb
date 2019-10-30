4e444d10f9256c7eca12d08ca47667739c325a10
Julia Bouvier <juliabouv@gmail.com>
2019-10-29 23:21:00 -0700

    Added custom method order_item and order tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,9 +4,17 @@ describe Order do
   let (:order1) {
     orders(:order1)
   }
-
+  
+  let (:order2) {
+    orders(:order2)
+  }
+  
   let (:pending_order) {
     orders(:pending_order)
+  }
+  
+  let (:wizard1) {
+    wizards(:wizard1)
   }
   
   it "can be instantiated" do
@@ -57,7 +65,7 @@ describe Order do
       expect(order1.valid?).must_equal true
       expect(order1.errors.messages).must_be_empty
     end
-
+    
     it "must have an email" do
       order1.email = nil
       
@@ -65,7 +73,7 @@ describe Order do
       expect(order1.errors.messages).must_include :email
       expect(order1.errors.messages[:email]).must_include "can't be blank"
     end
-
+    
     it "must have a valid email" do
       invalid_emails = ["peppermints.com", "muggle@domain", "applesauce"]
       invalid_emails.each do |invalid_email|
@@ -75,7 +83,7 @@ describe Order do
         expect(order1.errors.messages[:email]).must_include "is invalid"
       end
     end
-
+    
     it "does not require an name if status is pending" do
       order1.name = nil
       order1.status = 'pending'
@@ -99,7 +107,7 @@ describe Order do
       expect(order1.valid?).must_equal true
       expect(order1.errors.messages).must_be_empty
     end
-
+    
     it "must have an owling_address" do
       order1.owling_address = nil
       
@@ -107,7 +115,7 @@ describe Order do
       expect(order1.errors.messages).must_include :owling_address
       expect(order1.errors.messages[:owling_address]).must_equal ["can't be blank"]
     end
-
+    
     it "does not require a name_on_cc if status is pending" do
       order1.name_on_cc = nil
       order1.status = 'pending'
@@ -115,7 +123,7 @@ describe Order do
       expect(order1.valid?).must_equal true
       expect(order1.errors.messages).must_be_empty
     end
-
+    
     it "must have an name_on_cc" do
       order1.name_on_cc = nil
       
@@ -123,7 +131,7 @@ describe Order do
       expect(order1.errors.messages).must_include :name_on_cc
       expect(order1.errors.messages[:name_on_cc]).must_equal ["can't be blank"]
     end
-
+    
     it "does not require a cc_num if status is pending" do
       order1.cc_num = nil
       order1.status = 'pending'
@@ -139,7 +147,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_num
       expect(order1.errors.messages[:cc_num]).must_equal ["can't be blank"]
     end
-
+    
     it "does not require a cc_exp_mo if status is pending" do
       order1.cc_exp_mo = nil
       order1.status = 'pending'
@@ -155,7 +163,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_exp_mo
       expect(order1.errors.messages[:cc_exp_mo]).must_equal ["can't be blank"]
     end
-
+    
     it "must have a cc_exp_mo" do
       order1.cc_exp_mo = nil
       
@@ -163,7 +171,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_exp_mo
       expect(order1.errors.messages[:cc_exp_mo]).must_equal ["can't be blank"]
     end
-
+    
     it "does not require a cc_exp_yr if status is pending" do
       order1.cc_exp_yr = nil
       order1.status = 'pending'
@@ -179,7 +187,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_exp_yr
       expect(order1.errors.messages[:cc_exp_yr]).must_equal ["can't be blank"]
     end
-
+    
     it "does not require a cc_cvv if status is pending" do
       order1.cc_cvv = nil
       order1.status = 'pending'
@@ -195,7 +203,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_cvv
       expect(order1.errors.messages[:cc_cvv]).must_equal ["is not a number", "can't be blank"]
     end
-
+    
     it "cc_cvv must be number" do
       order1.cc_cvv = "not a number"
       
@@ -203,7 +211,7 @@ describe Order do
       expect(order1.errors.messages).must_include :cc_cvv
       expect(order1.errors.messages[:cc_cvv]).must_equal ["is not a number"]
     end
-
+    
     it "does not require a zip_code if status is pending" do
       order1.zip_code = nil
       order1.status = 'pending'
@@ -219,7 +227,7 @@ describe Order do
       expect(order1.errors.messages).must_include :zip_code
       expect(order1.errors.messages[:zip_code]).must_equal ["is not a number", "is the wrong length (should be 5 characters)", "can't be blank"]
     end
-
+    
     it "zip_code must be number" do
       order1.zip_code = "not a number"
       
@@ -227,7 +235,7 @@ describe Order do
       expect(order1.errors.messages).must_include :zip_code
       expect(order1.errors.messages[:zip_code]).must_equal ["is not a number", "is the wrong length (should be 5 characters)"]
     end
-
+    
     it "zip_code must be 5 length" do
       order1.zip_code = 1234
       
@@ -235,7 +243,7 @@ describe Order do
       expect(order1.errors.messages).must_include :zip_code
       expect(order1.errors.messages[:zip_code]).must_equal ["is the wrong length (should be 5 characters)"]
     end
-
+    
     it "must have a status" do
       order1.status = nil
       
@@ -244,38 +252,80 @@ describe Order do
       expect(order1.errors.messages[:status]).must_equal ["can't be blank"]
     end
   end
-
+  
   describe "Custom methods" do
     describe "total" do
       it "accurately calculates the total" do
         expect(order1.order_items).wont_be_empty
         expect(order1.total.amount).must_equal 3000 * 10**-2
       end
-
+      
       it "the total is 0 if there are no items" do
         OrderItem.delete_all
         expect(order1.order_items).must_be_empty
-
+        
         expect(order1.total).must_equal 0
       end
     end
+    
+    describe "wizard_items_total" do
+      it "accurately calculates the total" do
+        expect(order1.wizard_items_total(wizard1).amount).must_equal 3000 * 10**-2
+      end
+      
+      it "only includes order_items from that wizard in calculation" do
+        expect(order1.wizard_items_total(wizard1).amount).must_equal 3000 * 10**-2
+      end
+    end
+    
+    describe "complete_order" do
+      let (:order_items2) {
+        order_items(:order_items2)
+      }
+      
+      let (:order_items3) {
+        order_items(:order_items3)
+      }
 
+      it "can change order to complete" do
+        expect(order2.status).must_equal "paid"
+
+        order_items2.shipped = true
+        order_items2.save
+        order_items3.shipped = true
+        order_items3.save
+        
+        order2.complete_order
+        
+        expect(order2.status).must_equal "complete"
+      end
+      
+      it "does not change order to complete if even one item in order is not yet shipped" do
+        order_items3.shipped = true
+        order_items3.save
+        
+        order2.complete_order
+        
+        expect(order2.status).must_equal "paid"
+      end
+    end
+    
     describe "cancel_abandoned_orders" do
       it "can change an order to cancelled" do
         # expect(pending_order.status).must_equal 'pending'
-
+        
         # pending_order.created_at = DateTime.now - 5
         # Order.cancel_abandoned_orders
-
+        
         # expect(pending_order.status).must_equal 'cancelled'
       end
-
+      
       it "only changes orders that are a day old" do
-
+        
       end
-
+      
       it "only changes pending orders" do
-
+        
       end
     end
   end
