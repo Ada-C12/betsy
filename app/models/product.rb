8f929587272ad756649b8a62aa850faa01a9036b
dora1405 <li.dora05@gmail.com>
2019-10-31 19:15:38 -0700

    update product stock validation to be greater than zero

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,20 +3,20 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :nullify
   has_many :order_items, dependent: :nullify
   has_and_belongs_to_many :categories, dependent: :nullify
-
+  
   validates :name, presence: true
   validates_uniqueness_of :name
   validates :price_cents, presence: true
   validates :price_cents, numericality: { only_integer: true, greater_than: 0 }
   validates :stock, presence: true
-  validates :stock, numericality: { only_integer: true }
+  validates :stock, numericality: { only_integer: true, greater_than: 0 }
   validates :wizard, presence: true
   validates_inclusion_of :retired, in: [true, false]
-
+  
   monetize :price_cents
-
+  
   def self.five_products
     return Product.all.sample(5)
   end
-
+  
 end
